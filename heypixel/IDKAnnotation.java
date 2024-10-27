cf14d6c6bcbdda855a8b7d7b735abc25417c60ca
trychen <me@trychen.com>
2024-10-27 19:18:10 +0800

    clean：刪除狗屎代碼

--- heypixel/IDKAnnotation.java
+++
@@ -1,9 +0,0 @@
-package dev.undefinedteam.gensh1n.protocol.heypixel;
-
-import tech.skidonion.obfuscator.annotations.NativeObfuscation;
-import tech.skidonion.obfuscator.annotations.StringEncryption;
-
-
-
-public @interface IDKAnnotation {
-}
--- heypixel/data/retrievers/ObjectRetriever.java
+++
@@ -1,9 +0,0 @@
-package dev.undefinedteam.gensh1n.protocol.heypixel.data.retrievers;
-
-import tech.skidonion.obfuscator.annotations.NativeObfuscation;
-import tech.skidonion.obfuscator.annotations.StringEncryption;
-
-
-
-public interface ObjectRetriever extends DataRetriever, NumberValueRetriever {
-}
